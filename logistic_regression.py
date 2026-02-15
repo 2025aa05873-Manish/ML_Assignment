1225f28e7f6a3736bbd52341763917bafdf09c63
2025aa05873-Manish <2025aa05873@wilp.bits-pilani.ac.in>
2026-02-15 13:45:17 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -34,24 +34,6 @@ from statsmodels.tsa.stattools import adfuller
 from sklearn.preprocessing import MinMaxScaler
 
 
-# In[3]:
-
-
-from google.colab import drive
-
-# Mount Google Drive
-drive.mount('/content/drive')
-# List top-level contents of your Drive
-print(os.listdir("/content/drive/My Drive"))
-# Drill down step by step
-print(os.listdir("/content/drive/My Drive/"))
-print(os.listdir("/content/drive/My Drive/ML_Assignment2_Data"))
-# Access files
-data_path = '/content/drive/My Drive/ML_Assignment2_Data/'
-
-#df = pd.read_csv(data_path)
-
-
 # # Task
 # Load the 'Credit_card.csv' and 'Credit_card_label.csv' datasets from "/content/drive/My Drive/ML_Assignment2_Data/", preprocess the data, build and train a Logistic Regression model, and then evaluate its performance using Accuracy, AUC Score, Precision, Recall, F1 Score, and Matthews Correlation Coefficient.
 
@@ -336,4 +318,5 @@ print(f"Precision: {precision:.4f}")
 print(f"Recall: {recall:.4f}")
 print(f"F1 Score: {f1:.4f}")
 print(f"Matthews Correlation Coefficient (MCC): {mcc:.4f}")
+
 
